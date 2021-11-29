c04d4d8350fde07192d2d2b235c543b564be71be
hx1098 <1098269542@qq.com>
2021-11-29 13:48:50 +0800

    Initial commit

---
+++ nacos-9001/src/main/java/com/hx/nacos/Nacos9001Application.java
@@ -0,0 +1,13 @@
+package com.hx.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nacos9001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nacos9001Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/hx/alibabacloud/AlibabacloudApplication.java
@@ -0,0 +1,13 @@
+package com.hx.alibabacloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabacloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabacloudApplication.class, args);
+    }
+
+}
