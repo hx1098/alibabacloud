037fa77491e329185bbaaf7f2c41565d0c98d87c
hx1098 <1098269542@qq.com>
2021-12-09 17:25:16 +0800

    sentinel入门

--- sentinel-8401/src/main/java/com/hx/nacos/DemoController.java
+++ sentinel-8401/src/main/java/com/hx/nacos/DemoController.java
@@ -1,6 +1,6 @@
 package com.hx.nacos;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     @GetMapping("/testA")
+    @SentinelResource(value = "testA")
     public String testA() {
+        System.out.println("=====testA=====");
         return "===testA====";
     }
 
     @GetMapping("/testB")
+    @SentinelResource(value = "testB")
     public String testB() {
+        System.out.println("=====testB=====");
         return "===testB====";
     }
 
