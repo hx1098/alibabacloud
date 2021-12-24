6a78b96206da89bb8d39c7f29419995745c3b34e
hx1098 <1098269542@qq.com>
2021-12-24 22:21:55 +0800

    限流降级的时候跑出的错误

--- sentinel-8401/src/main/java/com/hx/nacos/DemoController.java
+++ sentinel-8401/src/main/java/com/hx/nacos/DemoController.java
@@ -1,9 +1,11 @@
 package com.hx.nacos;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -81,6 +83,38 @@ public class DemoController {
         }
         return testService.common();
     }
+
+
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "handlerHotKey",fallback = "testHotKeyThrowError")
+    public String testHotData(@RequestParam(value = "hot1",required = false)String hot1,
+                              @RequestParam(value = "hot2",required = false)String hot2,
+                              @RequestParam(value = "hot3",required = false)String hot3) {
+        //System.out.println(hot1);
+        if ("wangLiHong".equalsIgnoreCase(hot1)) {
+            throw new RuntimeException("某某某明星又上头条了， 这瓜包熟哇!!!");
+        }
+        /*int cc = 1 / 0;*/
+        return "testHotKey";
+    }
+
+    /**
+     * 限流降级的时候跑出的错误
+     * @return String
+     */
+    public String handlerHotKey(String hot1,String hot2, String hot3, BlockException e) {
+        return "由于这家伙流量太大, 程序员小哥正在抢修中......";
+    }
+
+    /**
+     * 报错有异常时候报出的错误
+     * @return String
+     */
+    public String testHotKeyThrowError(String hot1,String hot2, String hot3,Throwable e) {
+        return "这次接口是真的报错了!!!";
+    }
+
 
 
 
