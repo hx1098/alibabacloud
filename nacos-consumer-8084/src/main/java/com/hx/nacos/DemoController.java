598c7a0182a93387b056eb6083e5a2a55fc9825c
hx1098 <1098269542@qq.com>
2021-12-29 20:56:06 +0800

    处理sentinel限流 和 处理异常类型

--- nacos-consumer-8084/src/main/java/com/hx/nacos/DemoController.java
+++ nacos-consumer-8084/src/main/java/com/hx/nacos/DemoController.java
@@ -1,5 +1,7 @@
 package com.hx.nacos;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.hx.nacos.commons.JsonResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,9 +32,39 @@ public class DemoController {
 
 
     @GetMapping(value = "/consumer/fallback/{id}")
+    @SentinelResource(value = "fallback",fallback = "fallbackHandler",blockHandler = "blockHandler")
     public JsonResult<String> getServerPort(@PathVariable Long id) {
-        log.info("serverUrl::" + serverUrl);;
-        return restTemplate.getForObject(serverUrl + "/info/" + id, JsonResult.class);
+        log.info("serverUrl::" + serverUrl);
+        if (id <= 3) {
+            JsonResult forObject = restTemplate.getForObject(serverUrl + "/info/" + id, JsonResult.class);
+            System.out.println(forObject);
+            return forObject;
+        } else {
+            throw new NullPointerException("没有对应数据记录!");
+        }
+    }
+
+    /**
+     * 发生错误时候的提示
+     * 参数要和父方法的参数一样, 同时添加一个异常类型参数,
+     * @param id
+     * @param e
+     * @return
+     */
+    public JsonResult<String> fallbackHandler(Long id, Throwable e) {
+        JsonResult<String> result = new JsonResult<>(444,"出现未知商品id");
+        return result;
+    }
+
+    /**
+     * 出现限流产生的错误.处理sentinel限流
+     * @param id
+     * @param e
+     * @return
+     */
+    public JsonResult<String> blockHandler(Long id, BlockException e) {
+        JsonResult<String> result = new JsonResult<>(445, "BlockException限流");
+        return result;
     }
 
 
