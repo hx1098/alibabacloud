c7771a2dfa531233dd7fe06f7f5dd61975826a46
hx1098 <1098269542@qq.com>
2022-06-06 21:07:35 +0800

    gateway routes路由断言工厂, after在时间点之后可以使用

--- gateway-9999/src/main/java/com/hx/nacos/controller/DemoController.java
+++ gateway-9999/src/main/java/com/hx/nacos/controller/DemoController.java
@@ -1,6 +1,8 @@
 package com.hx.nacos.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
